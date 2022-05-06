8e0976ce5d80d5615128253e4929c644431fe53c
CristinaAvilaSantos <98969563+CristinaAvilaSantos@users.noreply.github.com>
2022-05-06 16:30:57 +0200

    extraccion de datos

---
+++ get_sample.py
@@ -0,0 +1,51 @@
+from pyspark import SparkContext, SparkConf
+
+from random import random
+FILES = [
+"201801_Usage_Bicimad.json",
+"201802_Usage_Bicimad.json",
+"201803_Usage_Bicimad.json",
+"201804_Usage_Bicimad.json",
+"201805_Usage_Bicimad.json",
+"201806_Usage_Bicimad.json",
+"201807_Usage_Bicimad.json",
+"201808_Usage_Bicimad.json",
+"201809_Usage_Bicimad.json",
+"201810_Usage_Bicimad.json",
+"201811_Usage_Bicimad.json",
+"201812_Usage_Bicimad.json",
+"202001_movements.json",
+"202002_movements.json",
+"202003_movements.json",
+"202004_movements.json",
+"202005_movements.json",
+"202006_movements.json",
+"202007_movements.json",
+"202008_movements.json",
+"202009_movements.json",
+"202010_movements.json",
+"202011_movements.json",
+"202012_movements.json"]
+
+
+def init_sc(num):
+    conf = SparkConf().setAppName(f'Bicimad {num}')
+    sc = SparkContext(conf=conf)
+    return sc
+    
+def main(sc, num):
+    rddlst = []
+    for f in FILES:
+        rddlst.append(sc.textFile(f'hdfs:///user/crisavil/{f}'))
+
+    rdd = sc.union(rddlst)
+    lst = rdd.takeSample(False, num)
+    with open(f"totData_{num}.json", 'w') as fout:
+        for data in lst:
+            fout.write(f'{data}\n')
+
+if __name__ == "__main__":
+    import sys
+    num = int(sys.argv[1])
+    with init_sc(num,) as sc:
+        main(sc, num)
