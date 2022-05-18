2b666d43ff91f8d9a1979caf70a55b35300fb1e3
CLAUDIA VIÑAS JÁÑEZ <cvinas@meralloz4.fdi.ucm.es>
2022-05-18 20:50:10 +0200

    mejora: bucle para mostrar tablas

--- my_BiciMAD.py
+++ my_BiciMAD.py
@@ -29,29 +29,43 @@ def get_weekday(hourtime):
 
 day_func = udf(lambda x: get_weekday(x), IntegerType())
 df = df.withColumn('get_weekday', day_func(df['unplug_hourTime']))
-df.show()
+#df.show()
 
 def get_year(hourtime):
-    """
-    Devuelve el año
-    """
     try:
         year = int(hourtime[:4])
-        month = int(hourtime[5:7])
-        day = int(hourtime[8:10])
-        return date(year, month, day).year()
+        return year
 
     except BaseException:
         year = int(hourtime[10:14])
-        month = int(hourtime[15:17])
-        day = int(hourtime[18:20])
-        return date(year, month, day).year()
+        return year
     
-year_func = udf(lambda x: get_year(x), IntegerType())  
+year_func = udf(lambda x: get_year(x), IntegerType())    
 df = df.withColumn('get_year', year_func(df['unplug_hourTime']))
 
 df2018 = df.filter(df["get_year"] == 2018)
-df2018.show()
+#df2018.show()
 df2020 = df.filter(df["get_year"] == 2020)
-df2020.show()
+#df2020.show()
 
+estaciones20 = df2020.groupBy(df2020['idunplug_station']).count()
+estaciones18 = df2018.groupBy(df2018['idunplug_station']).count()
+
+weekday18 = df2018.groupBy('get_weekday').count()
+weekday20 = df2020.groupBy('get_weekday').count()
+
+def tablas_edades(df):  #crea tablas para cada año y para cada rango de edad,  y las almacena en lista_tablas
+    lista_tablas = []
+    for i in range(7):
+        tabla = df.filter(df['ageRange'] == i)                 #edades2018_0 = df2018.filter(df2018['ageRange'] == 0)
+        tabla = tabla.groupBy(tabla['get_weekday']).count()    #edades2018_0 = edades2018_0.groupBy(edades2018_0['get_weekday']).count() saca el recuento por                                                                   dias de la semana
+        lista_tablas.append(tabla)
+    return lista_tablas
+
+def muestra_tablas_edades(df): #muestra las tablas 
+    tablas = tablas_edades(df)
+    for i in range(len(tablas)):
+        tablas[i].show()
+        
+muestra_tablas_edades(df2018)
+muestra_tablas_edades(df2020)
